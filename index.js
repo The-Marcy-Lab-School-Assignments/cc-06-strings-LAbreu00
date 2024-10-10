b44098a264ca98b2f5cfc9a3ad6fbf0081cb68bd
LAbreu00 <luisgabreu00@gmail.com>
2024-10-10 11:05:45 -0400

    finished Q2

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-//write your code here
+//Q1
+const reverseString = (s) => {
+  let e = "";
+  for (i = s.length - 1; i >= 0; i--) {
+    e += s[i];
+  }
+  return e;
+};
+
+//console.log(reverseString("hello"));
+//console.log(reverseString("oo"));
+//console.log(reverseString(""));
+
+//Q2
+const reverseZigZagString = (s) => {
+  let e = "";
+  for (i = s.length - 1; i >= 0; i--) {
+    if (i % 2 === 0) {
+      e += s[i].toLowerCase();
+    } else {
+      e += s[i].toUpperCase();
+    }
+  }
+  return e;
+};
+
+//console.log(reverseZigZagString("helloo"));
+//console.log(reverseZigZagString("Fellows"));
+//console.log(reverseZigZagString("Code Challenge"));
